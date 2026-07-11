afba1b40349c480f15317b8ba94c15bdc723c12d
emsallembenjamin <emsallem.benjamin@virtualtry.com>
2026-07-11 10:30:00 +0300

    fix: normalize JWT authentication failures

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -1,49 +1,46 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.js");
-const User = require("../models/user");
-const Role = require("../models/role");
 const UserService = require("../controllers/Database/account.js");
+const {
+  AuthorizationError,
+  copyIdentityClaims,
+  extractBearerToken,
+  sendAuthorizationError,
+} = require('../utils/authorization');
 
 const verifyToken = (req, res, next) => {
-  let token = req.headers["authorization"];
   try {
-    let token = req.headers["authorization"];
-    if (!token) {
-      return res.status(403).send({ message: "Authorization" });
-    }
-    let decode = jwt.verify(token, config.secret);
-    req.email = decode.email; 
-    req.accountUuid = decode.accountUuid; 
-    req.isEmailVerified = decode.isEmailVerified; 
-    req._id = decode._id;
-    next();
+    const token = extractBearerToken(req.get('authorization'));
+    const payload = jwt.verify(token, config.secret);
 
-  } catch (e) {
-    console.log(e);
-    return res.status(401).send({ message: "Unauthorized!" });
+    copyIdentityClaims(req, payload);
+    return next();
+  } catch (error) {
+    return sendAuthorizationError(res, error);
   }
 };
 
-const verifyTFAToken =async  (req, res, next) => {
-  let token = req.headers["authorization"];
-  console.log(token);
+const verifyTFAToken = async (req, res, next) => {
   try {
-    let token = req.headers["authorization"];
-    if (!token) {
-      return res.status(403).send({ message: "Authorization" });
+    const token = extractBearerToken(req.get('authorization'));
+    const payload = jwt.verify(token, config.tfa_secret);
+    const user = await UserService.getAccountDetailByEmail(payload.email);
+
+    if (!user) {
+      throw new AuthorizationError('Token account no longer exists');
     }
-    let decode = jwt.verify(token, config.tfa_secret);
-    req.email = decode.email; 
-    const user = await UserService.getAccountDetailByEmail(decode.email); 
-    req.user= user; 
-    next();
-  } catch (e) {
-    console.log(e);
-    return res.status(401).send({ message: "Unauthorized!" });
+
+    req.email = payload.email;
+    req.user = user;
+    return next();
+  } catch (error) {
+    return sendAuthorizationError(res, error);
   }
 };
 
 const authJwt = {
-  verifyToken, verifyTFAToken
+  verifyToken,
+  verifyTFAToken,
 };
+
 module.exports = authJwt;
